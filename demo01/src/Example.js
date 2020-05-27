5c88b39cbff597d4f65e656edd738f945074b2c5
quadrant26 <quadrant26@163.com>
2020-05-27 14:03:45 +0800

    react-hooks init

---
+++ demo01/src/Example.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+class Example extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { count: 0 }
+    }
+    render() { 
+        return (
+            <div>
+                <p>You click {this.state.count} times</p>
+                <button onClick={this.addCount.bind(this)}>click me</button>
+            </div>
+        );
+    }
+
+    addCount (){
+        this.setState({
+            count: this.state.count+1
+        })
+    }
+}
+ 
+export default Example;
---
+++ demo01/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Example from './Example-hooks'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Example />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
